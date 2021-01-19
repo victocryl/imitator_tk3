40f43d239c07d7c70796be45d58c53d96d901c43
victor <vikr1@yandex.ru>
2021-01-19 17:12:02 +0300

    расоложил основные виджеты на поле gui

--- widget.cpp
+++ widget.cpp
@@ -6,6 +6,7 @@ Widget::Widget(QWidget *parent)
     , ui(new Ui::Widget)
 {
     ui->setupUi(this);
+    this->setWindowTitle("имитатор СУ верхнего уровня для УК типа АК-3");
 }
 
 Widget::~Widget()
