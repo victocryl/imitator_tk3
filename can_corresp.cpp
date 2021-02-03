c4a9a6e117a108e214a00a6b2a767430e7da033d
Victor <vikr1@yandex.ru>
2021-02-03 13:45:20 +0300

    работает на приём и на передачу

--- can_corresp.cpp
+++ can_corresp.cpp
@@ -9,17 +9,17 @@ Can_corresp::Can_corresp(Ui::Widget *_ui, Can_init *_pobj_can_init)
 
     // инициализируем массивы can
     tx.resize(8);   // устанавливаем размер массива QByteArray tx
-    for(uint8_t i = 0; i < DATA_NUM; i++)   // memcpy нельзя использовать из-за проблем привидения даных
-    {tx[i] = 0;}
+    for(uint8_t i = 0; i < DATA_NUM; i++)   // memcpy нельзя использовать из-за проблем приведения даных
+    {tx[i] = 2;}
     uint8_t init_array[8] = {0,0,0,0,0,0,0,0};  // массив для инициализации
     memcpy(rx, init_array, 8);
 
 
 /* *********** коннекты ***********************************************/
 
-    // делаем отправку посылок по таймеру
-    connect(pobj_can_init->pobj_can_timer, SIGNAL(timeout()), this, SLOT(can_tx()));
-//    connect(pobj_can_init->device, SIGNAL(framesReceived()), this, SLOT(can_rx()));   // не могу понять, почему этот коннект здесь не работает
+
+    connect(pobj_can_init->pobj_can_timer, SIGNAL(timeout()), this, SLOT(can_tx()));    // отправка посылок по таймеру
+//    connect(pobj_can_init->device, SIGNAL(framesReceived()), this, SLOT(can_rx()));   // не могу понять, почему этот коннект здесь не работает (сделал в widget.cpp)
 
 
 
@@ -53,6 +53,9 @@ void Can_corresp::can_tx(void)
         frame_tx.setFrameId(ID_UKV);
         frame_tx.setPayload(tx);
         pobj_can_init->device->writeFrame(frame_tx);
+
+        pobj_can_init->device->clear(QCanBusDevice::Input);    // очищаем приёмный буфер контроллера can
+        // без этого буфер rx может заполняться длинной очередью LIFO и всё будет тормозить
         break;
     }
     default: break;
@@ -65,31 +68,25 @@ void Can_corresp::can_tx(void)
  */
 void Can_corresp::can_rx(void)
 {
-//    QCanBusFrame frame_rx = device->readFrame();
-//        QByteArray array = frame_rx.payload();
-//        char *data = array.data();  // массив-буфер
+    if(pobj_can_init->can_status)
+    {
+        QCanBusFrame frame_rx = pobj_can_init->device->readFrame();
+        QByteArray array = frame_rx.payload();
+        char *data = array.data();  // массив-буфер
 
-//        uint32_t tmp_id = frame_rx.frameId();   // считываем ID посылки.
+        uint32_t tmp_id = frame_rx.frameId();   // считываем ID посылки.
 
-//        switch(tmp_id)
-//        {
-//        case ID_UK_1:
-//            // перегрузка данных из масива-буфера в int-овый массив can_rx
-//            for(uint8_t i = 0; i < DATA_NUM; i++)
-//            {can_rx_UK1[i] = (uint8_t)(data[i]);}
-//            break;
-
-//        case ID_UK_2:
-//            // перегрузка данных из масива-буфера в int-овый массив can_rx
-//            for(uint8_t i = 0; i < DATA_NUM; i++)
-//            {can_rx_UK2[i] = (uint8_t)(data[i]);}
-//            break;
-
-    static int a = 0;
-    a++;
-    qDebug() << a;
-
-
+        // здесь идёт разбор посылок. в данном случае мы ждём только одну
+        switch(tmp_id)
+        {
+        case ID_H_L:
+            // перегрузка данных из массива-буфера char *data в int-овый массив rx
+            for(uint8_t i = 0; i < DATA_NUM; i++)
+            {rx[i] = (uint8_t)(data[i]);}
+            break;
+        default: break;
+        }
+    }
 }
 
 
--- widget.cpp
+++ widget.cpp
@@ -13,9 +13,10 @@ Widget::Widget(QWidget *parent)
 
     pobj_can_init->can_init();  // иницализируем канал can
 
+
     /************** КОННЕКТЫ ************************************/
     connect(ui->pushButton, SIGNAL(clicked(bool)), this->pobj_can_init, SLOT(can_channel_control()));  // управляем каналом CAN (подкл/откл)
-    connect(pobj_can_init->device, SIGNAL(framesReceived()), pobj_can_corresp, SLOT(can_rx())); // пытался сделать этот коннект в Can_corresp - не получилось
+    connect(pobj_can_init->device, SIGNAL(framesReceived()), pobj_can_corresp, SLOT(can_rx()));        // пытался сделать этот коннект в Can_corresp - не получилось
 
 
 
